9ed2b6a4c70a223914570f5eee6f8855fde33c48
Neville <nevil_dabre@yahoo.com>
2017-07-24 15:32:54 -0400

    https update

--- js/angularCode.js
+++ js/angularCode.js
@@ -3,7 +3,7 @@ var app = angular.module('mainApp', []);
 app.controller('myCtrl', function ($scope, $http) {
     $http({
         method: "GET",
-        url: "http://teamhortus.herokuapp.com/data"
+        url: "https://teamhortus.herokuapp.com/data"
     }).then(function mySuccess(response) {
         $scope.configurations = response.data[0];
     }, function myError(response) {
@@ -20,7 +20,7 @@ app.controller('editCtrl', function ($scope, $http) {
             method: "POST",
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
             data: $.param($scope.configuration),
-            url : "http://teamhortus.herokuapp.com/data"
+            url : "https://teamhortus.herokuapp.com/data"
         }).then(function mySuccess(response) {
             $scope.results = response.data;
             location.reload(); 
